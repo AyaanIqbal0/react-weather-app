f7d772d5c4bd4e1f6a19c7b77d80317fa4d6e6f6
Ayaan <ayaaniqbal403@gmail.com>
2024-09-05 10:29:10 +0530

    bugs fixed:

--- src/components/TopButtons.jsx
+++ src/components/TopButtons.jsx
@@ -1,26 +1,25 @@
 const TopButtons = ({ setQuery }) => {
-    const cities = [
-      { id: 1, name: "Varanasi" },
-      { id: 2, name: "Delhi" },
-      { id: 3, name: "Mumbai" },
-      { id: 4, name: "Tokyo" },
-      { id: 5, name: "London" },
-    ];
-  
-    return (
-      <div className="flex flex-wrap items-center justify-center my-6">
-        {cities.map((city) => (
-          <button
-            key={city.id}
-            className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in m-1"
-            onClick={() => setQuery({ q: city.name })}
-          >
-            {city.name}
-          </button>
-        ))}
-      </div>
-    );
-  };
-  
-  export default TopButtons;
-  
+  const cities = [
+    { id: 1, name: "Varanasi" },
+    { id: 2, name: "Delhi" },
+    { id: 3, name: "Mumbai" },
+    { id: 4, name: "Tokyo" },
+    { id: 5, name: "London" },
+  ];
+
+  return (
+    <div className="flex flex-wrap items-center justify-center my-6">
+      {cities.map((city) => (
+        <button
+          key={city.id}
+          className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in m-1"
+          onClick={() => setQuery({ q: city.name })}
+        >
+          {city.name}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default TopButtons;
